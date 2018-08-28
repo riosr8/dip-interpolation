e1c22bff44fc9b65064d20fd41a4dd465ee2d36a
zhenggang65 <zhenggangli65@gmail.com>
2018-08-28 16:13:08 -0500

    create first assignment

---
+++ resize/resample.py
@@ -0,0 +1,41 @@
+
+
+class resample:
+    def resize(self, image, fx=None, fy=None, interpolation=None):
+        """calls the appropriate funciton to resample an image based on the interpolation method
+        image: the image to be resampled
+        fx: scale along x direction (eg. 0.5, 1.5, 2.5)
+        fx: scale along y direction (eg. 0.5, 1.5, 2.5)
+        interpolation: method used for interpolation ('either bilinear or nearest_neighbor)
+        returns a resized image based on the interpolation method
+        """
+        if interpolation == 'bilinear':
+            return self.bilinear_interpolation(image, float(fx), float(fy))
+
+        elif interpolation == 'nearest_neighbor':
+            return self.nearest_neighbor(image, float(fx), float(fy))
+
+    def nearest_neighbor(self, image, fx, fy):
+        """resizes an image using bilinear interpolation approximation for resampling
+        image: the image to be resampled
+        fx: scale along x direction (eg. 0.5, 1.5, 2.5)
+        fx: scale along y direction (eg. 0.5, 1.5, 2.5)
+        returns a resized image based on the nearest neighbor interpolation method
+        """
+
+        # Write your code for nearest neighbor interpolation here
+
+
+        return image
+
+    def bilinear_interpolation(self, image, fx, fy):
+        """resizes an image using bilinear interpolation approximation for resampling
+        image: the image to be resampled
+        fx: scale along x direction (eg. 0.5, 1.5, 2.5)
+        fx: scale along y direction (eg. 0.5, 1.5, 2.5)
+        returns a resized image based on the bilinear interpolation method
+        """
+
+        # Write your code for bilinear interpolation here
+
+        return image
